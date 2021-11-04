ef4d5ac7165c81555d6cf31d9abb36ce342c5482
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-11-03 19:46:35 -0500

    17 commit

--- microservicio/dominio/src/main/java/com/ceiba/espacio/modelo/dto/DtoEspacio.java
+++ microservicio/dominio/src/main/java/com/ceiba/espacio/modelo/dto/DtoEspacio.java
@@ -1,28 +1,16 @@
 package com.ceiba.espacio.modelo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-
-
+@Getter
+@AllArgsConstructor
 public class DtoEspacio {
     private Long id;
     private String nombre;
     private String estado;
 
-    public DtoEspacio(Long id, String nombre, String estado) {
-        this.id = id;
-        this.nombre = nombre;
-        this.estado = estado;
-    }
 
-    public Long getId() {
-        return id;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public String getEstado() {
-        return estado;
-    }
 }
--- microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
+++ microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
@@ -1,9 +1,11 @@
 package com.ceiba.vehiculo.modelo.dto;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
-
+@Getter
+@AllArgsConstructor
 public class DtoVehiculo {
     private Long id;
     private String placa;
@@ -17,64 +19,5 @@ public class DtoVehiculo {
     private Double totalPagar;
     private Double precioBaseHora;
 
-    public DtoVehiculo(Long id, String placa, Long idEspacio, String tipoVehiculo,
-                       String modeloVehiculo, String nombrePropietario,
-                       String apellidoPropietario, LocalDateTime fechaEntrada,
-                       LocalDateTime fechaSalida, Double totalPagar, Double precioBaseHora) {
-        this.id = id;
-        this.placa = placa;
-        this.idEspacio = idEspacio;
-        this.tipoVehiculo = tipoVehiculo;
-        this.modeloVehiculo = modeloVehiculo;
-        this.nombrePropietario = nombrePropietario;
-        this.apellidoPropietario = apellidoPropietario;
-        this.fechaEntrada = fechaEntrada;
-        this.fechaSalida = fechaSalida;
-        this.totalPagar = totalPagar;
-        this.precioBaseHora = precioBaseHora;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
-    public Long getIdEspacio() {
-        return idEspacio;
-    }
-
-    public String getTipoVehiculo() {
-        return tipoVehiculo;
-    }
-
-    public String getModeloVehiculo() {
-        return modeloVehiculo;
-    }
-
-    public String getNombrePropietario() {
-        return nombrePropietario;
-    }
-
-    public String getApellidoPropietario() {
-        return apellidoPropietario;
-    }
-
-    public LocalDateTime getFechaEntrada() {
-        return fechaEntrada;
-    }
-
-    public LocalDateTime getFechaSalida() {
-        return fechaSalida;
-    }
-
-    public Double getTotalPagar() {
-        return totalPagar;
-    }
-
-    public Double getPrecioBaseHora() {
-        return precioBaseHora;
-    }
 }
