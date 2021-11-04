d363c029e3748eb3f68ff33aa50fc60cb3d29f0f
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-11-03 20:23:33 -0500

    24 commit

--- microservicio/dominio/src/main/java/com/ceiba/espacio/modelo/dto/DtoEspacio.java
+++ microservicio/dominio/src/main/java/com/ceiba/espacio/modelo/dto/DtoEspacio.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class DtoEspacio {
     private Long id;
     private String nombre;
     private String estado;
 
-
-
-
+    public DtoEspacio(Long id, String nombre, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
 }
