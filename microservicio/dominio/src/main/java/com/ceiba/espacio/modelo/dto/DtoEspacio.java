d3705d3db2bebcaaba2fe0ed5283383fb5bab73d
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-11-03 20:27:14 -0500

    25 commit

--- microservicio/dominio/src/main/java/com/ceiba/espacio/modelo/dto/DtoEspacio.java
+++ microservicio/dominio/src/main/java/com/ceiba/espacio/modelo/dto/DtoEspacio.java
@@ -4,14 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class DtoEspacio {
     private Long id;
     private String nombre;
     private String estado;
-
-    public DtoEspacio(Long id, String nombre, String estado) {
-        this.id = id;
-        this.nombre = nombre;
-        this.estado = estado;
-    }
 }
--- microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
+++ microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 @Getter
-
+@AllArgsConstructor
 public class DtoVehiculo {
     private Long id;
     private String placa;
@@ -18,22 +18,4 @@ public class DtoVehiculo {
     private LocalDateTime fechaSalida;
     private Double totalPagar;
     private Double precioBaseHora;
-
-    public DtoVehiculo(Long id, String placa, Long idEspacio,
-                       String tipoVehiculo, String modeloVehiculo,
-                       String nombrePropietario, String apellidoPropietario,
-                       LocalDateTime fechaEntrada, LocalDateTime fechaSalida,
-                       Double totalPagar, Double precioBaseHora) {
-        this.id = id;
-        this.placa = placa;
-        this.idEspacio = idEspacio;
-        this.tipoVehiculo = tipoVehiculo;
-        this.modeloVehiculo = modeloVehiculo;
-        this.nombrePropietario = nombrePropietario;
-        this.apellidoPropietario = apellidoPropietario;
-        this.fechaEntrada = fechaEntrada;
-        this.fechaSalida = fechaSalida;
-        this.totalPagar = totalPagar;
-        this.precioBaseHora = precioBaseHora;
-    }
 }
