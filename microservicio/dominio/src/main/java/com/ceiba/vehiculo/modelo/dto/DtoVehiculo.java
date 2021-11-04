98f3236999f67190ef90e93db01bbe6a89042700
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-11-03 20:19:10 -0500

    23 commit

--- microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
+++ microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
@@ -1,9 +1,11 @@
 package com.ceiba.vehiculo.modelo.dto;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
-
+@Getter
+@AllArgsConstructor
 public class DtoVehiculo {
     private Long id;
     private String placa;
@@ -16,31 +18,4 @@ public class DtoVehiculo {
     private LocalDateTime fechaSalida;
     private Double totalPagar;
     private Double precioBaseHora;
-
-    public DtoVehiculo(Long id, String placa, Long idEspacio, String tipoVehiculo,
-                       String modeloVehiculo, String nombrePropietario,
-                       String apellidoPropietario, LocalDateTime fechaEntrada,
-                       LocalDateTime fechaSalida, Double totalPagar, Double precioBaseHora) {
-        this.id = id;
-        this.placa = placa;
-        this.idEspacio = idEspacio;
-        this.tipoVehiculo = tipoVehiculo;
-        this.modeloVehiculo = modeloVehiculo;
-        this.nombrePropietario = nombrePropietario;
-        this.apellidoPropietario = apellidoPropietario;
-        this.fechaEntrada = fechaEntrada;
-        this.fechaSalida = fechaSalida;
-        this.totalPagar = totalPagar;
-        this.precioBaseHora = precioBaseHora;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
-
 }
