16582973300dc9ec42f937e89ae7f43328951583
Juan Pablo Caro Vargas - Ceiba Software <juan.caro@ceiba.com.co>
2021-11-03 20:07:40 -0500

    21 commit

--- microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
+++ microservicio/dominio/src/main/java/com/ceiba/vehiculo/modelo/dto/DtoVehiculo.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-@Getter
-@AllArgsConstructor
+
 public class DtoVehiculo {
     private Long id;
     private String placa;
@@ -19,5 +18,64 @@ public class DtoVehiculo {
     private Double totalPagar;
     private Double precioBaseHora;
 
+    public DtoVehiculo(Long id, String placa, Long idEspacio, String tipoVehiculo,
+                       String modeloVehiculo, String nombrePropietario,
+                       String apellidoPropietario, LocalDateTime fechaEntrada,
+                       LocalDateTime fechaSalida, Double totalPagar, Double precioBaseHora) {
+        this.id = id;
+        this.placa = placa;
+        this.idEspacio = idEspacio;
+        this.tipoVehiculo = tipoVehiculo;
+        this.modeloVehiculo = modeloVehiculo;
+        this.nombrePropietario = nombrePropietario;
+        this.apellidoPropietario = apellidoPropietario;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        this.totalPagar = totalPagar;
+        this.precioBaseHora = precioBaseHora;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public Long getIdEspacio() {
+        return idEspacio;
+    }
+
+    public String getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public String getModeloVehiculo() {
+        return modeloVehiculo;
+    }
+
+    public String getNombrePropietario() {
+        return nombrePropietario;
+    }
+
+    public String getApellidoPropietario() {
+        return apellidoPropietario;
+    }
+
+    public LocalDateTime getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public LocalDateTime getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public Double getTotalPagar() {
+        return totalPagar;
+    }
+
+    public Double getPrecioBaseHora() {
+        return precioBaseHora;
+    }
 }
